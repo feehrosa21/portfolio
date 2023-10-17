eb054dc858fcdfacfb59f539e833092064c1be7b
Fernanda Rosa <115740376+feehrosa21@users.noreply.github.com>
2023-10-17 00:27:57 -0300

    atualizações portfólio

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@ const menu = document.querySelector('.menu');
 const navMenu = document.querySelector('.nav-menu');
 const content = document.getElementById('content');
 var icon = document.getElementById('mode');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
+/*menu hamburguer*/
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     navMenu.classList.toggle('ativo');
 })
 
+/*remover menu hamburguer ao clicar no conteúdo*/
 content.addEventListener('click', () =>{
     if(navMenu.classList.contains('ativo')) {
         menu.classList.remove('ativo');
@@ -15,7 +19,24 @@ content.addEventListener('click', () =>{
     }
 });
 
+/*link ativo menu de navegação*/
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+/*modo escuro/claro*/
 icon.onclick = function(){
     document.body.classList.toggle('dark-mode');
     if(document.body.classList.contains('dark-mode')){
@@ -25,6 +46,7 @@ icon.onclick = function(){
     }
 }
 
+/* efeitos ao atualizar a página */
 window.sr = ScrollReveal({ reset: true });
 
 sr.reveal('.name', {duration:2000});
@@ -32,7 +54,7 @@ sr.reveal('.img-port', {duration: 1000});
 sr.reveal('.h2,.h3, .h4', {duration: 500});
 sr.reveal('#icon-h', {duration: 500}); 
 
-
+/*menu portfólio*/
 jQuery(document).ready(function() {
         jQuery('#tab1').css('display', 'block');
 
@@ -43,4 +65,10 @@ jQuery(document).ready(function() {
                 jQuery(id).css('display', 'block');
         }); 
     });
+
+/* link ativo menu navegação portfólio*/     
+$('.link-a').click(function(e) {
+    $('.link-a').removeClass('active-port');
+    $(this).addClass('active-port');
+  });
 
